62db0a56282894b94d0aab9a3301ae645df7c908
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-23 23:59:22 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-/*resource "google_project_iam_binding" "project" {
+resource "google_project_iam_binding" "project" {
   project = "secret-epsilon-359902"
   role    = "roles/viewer"
 
@@ -6,4 +6,14 @@
     "user:gcptrial02@gmail.com",
     "user:tejaswivanga.0303@gmail.com"
   ]
-}*/
+}
+
+resource "google_project_iam_binding" "project" {
+  project = "secret-epsilon-359902"
+  role    = "roles/editor"
+
+  members = [
+    "user:gcptrial02@gmail.com",
+    "user:tejaswivanga.0303@gmail.com"
+  ]
+}
