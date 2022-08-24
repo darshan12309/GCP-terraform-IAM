33d014f36ccf0c3de9f3f0508563314fe6be7b03
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-24 07:43:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
-module "iam" {
+/*module "iam" {
   source = "./modules"
   for_each           = local.iam_details
 
    role = each.value.role
    members = each.value.members
+}*/
+
+  module "iam" {
+  source = "./modules"
 }
