eedddf74129169614c596009a29de677086b7ff2
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-24 07:59:01 +0530

    Update iam.tf

--- modules/iam.tf
+++ modules/iam.tf
@@ -1,5 +1,5 @@
 resource "google_project_iam_binding" "project" {
-  project = "secret-epsilon-359902"
+  project = var.project_id
 
   role    = var.role #"roles/editor"
   members = var.members #"user:gcptrial02@gmail.com"
