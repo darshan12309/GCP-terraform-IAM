5764bffb8876d24d6074bfb581646fe20a250606
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-24 07:15:34 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -5,25 +5,3 @@ resource "google_project_iam_binding" "project" {
   role    = each.value.role #"roles/editor"
   members = each.value.members #"user:gcptrial02@gmail.com"
 }
-
-
-/*resource "google_project_iam_binding" "project" {
-  project = "secret-epsilon-359902"
-  role    = "roles/viewer"
-
-  members = [
-    "user:gcptrial02@gmail.com",
-    "user:tejaswivanga.0303@gmail.com"
-    "serviceAccount:service-acc-1@secret-epsilon-359902.iam.gserviceaccount.com"
-  ]
-}
-
-resource "google_project_iam_binding" "project" {
-  project = "secret-epsilon-359902"
-  role    = "roles/editor"
-
-  members = [
-    "user:gcptrial02@gmail.com",
-    "user:tejaswivanga.0303@gmail.com"
-  ]
-}*/
