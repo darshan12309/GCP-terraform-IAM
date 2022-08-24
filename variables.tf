ff69bb72e029d39e86bdb5bbba92a6d07ac08e3a
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-24 07:06:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+locals {
+
+    iam_details = {
+
+    "iam_details"  = {
+          role = "roles/editor"
+          members = [
+            "user:gcptrial02@gmail.com" , 
+            "user:tejaswi.vanga030@gmail.com"
+          ]
+        }
+    }
+}
