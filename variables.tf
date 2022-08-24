f61fffdb06c89e298d668742f1dfc0f3d47c4df1
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-24 07:33:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
 locals {
   iam_details = {
-    "iam_details"  = {
+    "iam_details_1"  = {
           role = "roles/editor"
+          members = [
+            "user:gcptrial02@gmail.com" , 
+            "user:tejaswi.vanga0303@gmail.com"
+          ]
+        }
+     "iam_details_2"  = {
+          role = "roles/compute.admin"
           members = [
             "user:gcptrial02@gmail.com" , 
             "user:tejaswi.vanga0303@gmail.com"
