5b920ffeda23d65673a678f9d3870210ca7dc7fa
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-26 23:35:13 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,14 +1,14 @@
 locals {
   iam_details = {
     "role_1"  = {
-          role = "roles/editor"
+          role = "roles/pubsub.admin"
           members = [
             "user:gcptrial02@gmail.com" , 
           ]
         }
     #============================================================#
      "role_2"  = {
-          role = "roles/compute.admin"
+          role = "roles/bigquery.admin"
           members = [
             "user:gcptrial02@gmail.com" , 
             "user:tejaswi.vanga0303@gmail.com"
@@ -16,7 +16,7 @@ locals {
         }
     #============================================================#
      "role_3"  = {
-          role = "roles/storage.admin"
+          role = "roles/notebooks.admin"
           members = [
             "user:gcptrial02@gmail.com" , 
             "user:tejaswi.vanga0303@gmail.com"
