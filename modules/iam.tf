68ad8330a3e7e5887a7623c891d3345fd5bf84ef
darshan12309 <76704105+darshan12309@users.noreply.github.com>
2022-08-26 23:27:55 +0530

    Update iam.tf

--- modules/iam.tf
+++ modules/iam.tf
@@ -1,6 +1,6 @@
-/*resource "google_project_iam_binding" "project" {
+resource "google_project_iam_binding" "project" {
   project = var.project_id
 
   role    = var.role #"roles/editor"
   members = var.members #"user:gcptrial02@gmail.com"
-}*/
+}
